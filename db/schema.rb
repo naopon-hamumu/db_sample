c31b229e00bd4b7df3ecd870f7cdf94c31a0ace4
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2024-02-02 15:38:22 +0900

    3-3-2 テーブルの定義を反映させる

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_02_060754) do
+  create_table "blogs", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "blog_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_entries_on_blog_id"
+  end
+
+  add_foreign_key "entries", "blogs"
 end
